965d0e4aeddb81b60e79ff48e30504ede360091a
webreznov-studio <webreznov.landing@gmail.com>
2021-11-11 09:32:05 +0300

    hide menu

--- src/components/Header/components/LogoMenu/LogoMenu.jsx
+++ src/components/Header/components/LogoMenu/LogoMenu.jsx
@@ -1,17 +1,18 @@
 import React, {useState} from "react";
 import {Link} from "gatsby";
 import logo from "../../../../images/logo.png";
-import {MenuList} from "../MenuList/MenuList";
+// import {MenuList} from "../MenuList/MenuList";
 
 export const LogoMenu = () => {
     const [isShow, setIsShow] = useState(false);
 
     return (
-        <div onClick={() => setIsShow(!isShow)}>
+        <div onClick={() => setIsShow(false)}>
+        {/* <div onClick={() => setIsShow(!isShow)}> */}
             <Link to="#">
                 <img src={logo} alt="" />
             </Link>
-            {isShow && <MenuList onClose={setIsShow} />}
+            {/* {isShow && <MenuList onClose={setIsShow} />} */}
         </div>
     )
 }
